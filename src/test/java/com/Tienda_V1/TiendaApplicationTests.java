676f77391a3a4e603cdaa255bd73f4b1401cc27a
Paula <Paula@DESKTOP-2C757CK>
2023-11-08 13:40:38 -0600

    Se agregan los archivos

---
+++ src/test/java/com/Tienda_V1/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_V1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
